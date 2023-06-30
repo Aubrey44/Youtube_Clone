7e4253088a61feee646ced40931224d295b0c63a
Andrew Witmer <witmer.a@gmail.com>
2023-06-30 09:28:39 -0700

    Completed app models, performed migrations

--- backend/reply/models.py
+++ backend/reply/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 from authentication.models import User
+from comment.models import Comment
 
 # Create your models here.
 
-class Comment(models.Model):
+class Reply(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    comment = models.ForeignKey(on_delete=models.CASCADE)
-    text = models.CharField(max_length=200)
+    comment = models.ForeignKey(Comment, on_delete=models.CASCADE)
+    text = models.CharField(max_length=255)
